2ea07623e522386fd943fe5a4f5ace09df717017
Alan <alanmak95@gmail.com>
2021-01-07 22:10:57 +0000

    Update search and owner listing with SQL queries

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -80,7 +80,6 @@ const getAllReservations = function(guest_id, limit = 10) {
   `, [guest_id, limit])
   .then (res => res.rows)
   .catch (err => console.log("ERROR", err))
-  // return getAllProperties(null, 2);
 }
 exports.getAllReservations = getAllReservations;
 
@@ -93,10 +92,42 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = function(options, limit = 10) {
-  return pool.query(`
-  SELECT * FROM properties
-  LIMIT $1
-  `, [limit])
+  const queryParams = [];
+  let queryString = `
+  SELECT properties.*, avg(property_reviews.rating) as average_rating
+  FROM properties
+  JOIN property_reviews ON properties.id = property_id
+  WHERE 1=1
+  `;
+    //Loop
+    if (options.owner_id) {
+      queryParams.push(parseInt(`${options.owner_id}`))
+      queryString += `AND properties.owner_id = $${queryParams.length}`
+    }
+    if (options.city) {
+      queryParams.push(`%${options.city}%`);
+      queryString += `AND city LIKE $${queryParams.length} `;
+    }
+    if (options.minimum_rating) {
+      queryParams.push(`${options.minimum_rating}`)
+      queryString += `AND rating >= $${queryParams.length}`
+    }
+    if (options.minimum_price_per_night) {
+      queryParams.push(`${options.minimum_price_per_night || 0}`);
+      queryString += `AND cost_per_night >= $${queryParams.length}`
+    }
+    if(options.maximum_price_per_night) {
+        queryParams.push(`${options.maximum_price_per_night}`);
+        queryString += `AND cost_per_night <= $${queryParams.length}`
+    }
+  queryParams.push(limit);
+  queryString += `
+  GROUP BY properties.id
+  ORDER BY cost_per_night
+  LIMIT $${queryParams.length};
+  `;
+
+  return pool.query(queryString, queryParams)
   .then(res => res.rows);
 }
 exports.getAllProperties = getAllProperties;
